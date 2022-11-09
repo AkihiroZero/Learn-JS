e8398759612af38074c6b2c3e43c8d13dae585e4
AkihiroZero <akihirozero02@gmail.com>
2022-11-09 16:01:08 +0700

    first commit

---
+++ learn-js.js
@@ -0,0 +1,38 @@
+//1). Comment
+// Example Comment
+
+/*2). Paragraph Comment
+ Another Example Comment
+ */
+
+//3). Basic show data type string
+document.writeln("String using Src Script");
+document.writeln("<br>");
+document.writeln('String using Src Script single quotation mark');
+
+//4). Basic show data type number
+document.writeln(100);
+document.writeln("<br>");
+document.writeln(100.123);
+document.writeln("<br>");
+document.writeln(0b11111010);
+ 
+//5). Basic show data type boolean
+document.writeln(true); //true condition
+document.writeln("<br>");
+document.writeln(false); //false condition
+document.writeln("<br>");
+
+//6). Double String shows/string concenate
+document.writeln("Double :" + "String");
+document.writeln("<br>");
+document.writeln('Multi' + 'triple' + 'string'); 
+document.writeln("<br>");
+
+//7). Escape sequence
+document.write("<textarea cols='100' rows='10'>");
+document.write("\nEnter \nSequence \n");
+document.write("\\Double \\Slash \n");
+document.write("\"Qutation Mark String\"\n");
+document.write("");
+document.write("</textarea>");
