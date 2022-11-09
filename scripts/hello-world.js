93e55d57744564f89ed7f86f2ec06b750cb65d00
AkihiroZero <akihirozero02@gmail.com>
2022-11-09 07:25:55 +0700

    first commit

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,1 +1,22 @@
-document.writeln("Hellow world using Script")
+//1). Comment
+// Example Comment
+
+/*2). Paragraph Comment
+ Another Example Comment
+ */
+
+//3). Basic show data type string
+document.writeln("Hello world using Src Script");
+document.writeln("<br>");
+
+//4). Basic show data type number
+document.writeln(100);
+document.writeln("<br>");
+document.writeln(100.123);
+document.writeln("<br>");
+document.writeln(0b11111010);
+ 
+//5). Basic show data type boolean
+document.writeln(true) //true condition
+document.writeln("<br>");
+document.writeln(false) //false condition
