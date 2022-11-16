33b5f586a8aecacfb6f210eefe392fb1fb200d48
AkihiroZero <akihirozero02@gmail.com>
2022-11-16 20:19:48 +0700

    Third Commit

--- learn-js-basic-function.js
+++ learn-js-basic-function.js
@@ -39,3 +39,73 @@ document.writeln("<br>");
 document.writeln(isNaN(second));
 document.writeln("<hr>");
 
+//2). If Expression
+
+const ifExample = 90;
+
+if(ifExample > 80){
+    document.writeln("You Passed If Condition")
+    document.writeln("<br>");
+
+}
+if(ifExample < 100){
+    document.writeln("Another Passed If Condition")
+    document.writeln("<br>");
+}
+
+//3) Else Expression
+
+const elseExample = 75;
+
+if (elseExample > 80) {
+    document.writeln("Wont Show Cause its false"); 
+}
+else{
+    document.writeln("Its Else Condition");
+    document.writeln("<hr>");
+}
+
+//4) ElseIf Expression
+
+const elseIfExample = 80;
+
+if (elseIfExample < 75) {
+    document.writeln("If Expression");    
+}else if (elseIfExample > 75) {
+    document.writeln("Its Else If Expression");
+    document.writeln("<hr>");
+}else{
+    document.writeln("Else Expression");
+}
+
+//5). Switch Expression
+
+const switchExample = "B";
+
+switch (switchExample) {
+    case "A":
+        document.writeln("A Switch Case 1 Expression");
+        document.writeln("<br>");
+        break;
+    case "B":
+        document.writeln("Another Switch Case");
+        document.writeln("<hr>");
+    default:
+        break;
+}
+
+//6). Type Off
+
+const typeoffString = "String";
+document.writeln(`${typeof typeoffString}`);
+document.writeln("<br>");
+const typeoffBoolean = true;
+document.writeln(`${typeof typeoffBoolean}`);
+document.writeln("<br>");
+const typeoffNumber = 100;
+document.writeln(`${typeof typeoffNumber}`);
+document.writeln("<br>");
+const typeoffObject = {};
+document.writeln(`${typeof typeoffObject}`);
+document.writeln("<hr>");
+
